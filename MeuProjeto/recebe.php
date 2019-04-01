17c4b88a80d9b4d10bced96dae59eb5da5c1df74
pkthegod <38172122+pkthegod@users.noreply.github.com>
2019-04-01 20:51:58 -0300

    Add files via upload

---
+++ MeuProjeto/recebe.php
@@ -0,0 +1,29 @@
+
+        <?php
+        // put your code here
+        $servidor = "localhost";
+        $user = "root";
+        $passwd = "";
+        $database = "cadastro0104";
+        
+        $conecta = new mysqli($servidor,$user,$passwd,$database);
+        
+        if (!$conecta){
+            die("Conexão falhou: " .$conecta->connect_error."<br>");
+        }
+        echo "Conexão realizada com sucesso";
+        
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+        
+        $sql = "INSERT INTO clientes(nome, email, senha)
+        VALUES ('$nome', '$email', '$senha')";
+        if ($conecta->query($sql) === TRUE) {
+            echo "Novo registro criado com sucesso";
+        } else {
+            echo "Erro: " .$sql."<br>".$conecta->error."<br>";
+        }
+        $conecta->close();
+        ?>
+
